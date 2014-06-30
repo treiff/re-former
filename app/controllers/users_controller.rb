ac8d0d305d3a3a27a3bef7fcaafbb69a9af01376
treiff <trevorreiff@gmail.com>
2014-06-29 21:45:47 -0400

    finished HTML forms, beginning on converting to rails form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 	def new
+		@user = User.new 
 	end
 
 	def create
-		#@user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-		@user = User.new(user_params)
+		@user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+		#@user = User.new(user_params)
 		
 		if @user.save
 			redirect_to new_user_path
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
 
 	private
 
-	def user_params
-		params.require(:user).permit(:username, :email, :password)
-	end
+	#def user_params
+		#params.require(:user).permit(:username, :email, :password)
+	#end
 end
