8c01e0215f1131431954d9ef4fb3dd660f4b2647
Trevor Reiff <trevorreiff@gmail.com>
2014-12-31 16:40:11 -0500

    update readme, cleanup code.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * * *
 
 This is part of the Forms Project in The Odin Project's Ruby on Rails Curriculum. Find it [here.](http://www.theodinproject.com)
+
+Practice working on form_for helper along with understanding parameters.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,38 @@
 class UsersController < ApplicationController
-	def new
-		@user = User.new 
-	end
+  def new
+    @user = User.new 
+  end
 
-	def create
-		@user = User.new(user_params)
-		
-		if @user.save
-			redirect_to new_user_path
-		else
-			render :new
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "Save worked!"
+      redirect_to new_user_path
+    else
+      flash.now[:error] = "That's not going to work."
+      render :new
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
-			flash[:success] = "Yay! Updated!"
-			redirect_to edit_user_path(@user)
-		else
-			flash.now[:error] = "You messed up! try again."
-			render :edit
-		end
-	end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Yay! Updated!"
+      redirect_to edit_user_path(@user)
+    else
+      flash.now[:error] = "You messed up! try again."
+      render :edit
+    end
+  end
 
-	private
+  private
 
-	def user_params
-		params.require(:user).permit(:username, :email, :password)
-	end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
